259464f55a04920f73575d90811cdab04ee2e837
Alessandro <105470745+Alessito54@users.noreply.github.com>
2026-09-19 22:16:19 -0600

    fix(db): add check constraints to enforce domain rules

--- backend/dist/vehiculos/domain/Vehiculo.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EstadoVehiculo = void 0;
-var EstadoVehiculo;
-(function (EstadoVehiculo) {
-    EstadoVehiculo["DISPONIBLE"] = "DISPONIBLE";
-    EstadoVehiculo["EN_USO"] = "EN_USO";
-    EstadoVehiculo["MANTENIMIENTO"] = "MANTENIMIENTO";
-})(EstadoVehiculo || (exports.EstadoVehiculo = EstadoVehiculo = {}));
